cc0ee1b46d6341b8ba5739da73d82193e90d7e90
Steve Thompson <writetome51@gmail.com>
2017-12-06 08:14:42 -0700

    new commit

--- golf-courses.js
+++ golf-courses.js
@@ -1,19 +1,54 @@
-var closeCourses;
+var courses;
 var local_obj = {latitude:40.4426135, longitude:-111.8631116, radius:100};
+var courseNames=[];
+var courseOptions='';
+var teeOptions= '';
+var courseHref = '';
+var teeYardages;
 
-function loadMe(){
+
+loadCourses();
+
+
+function loadCourses(){
 	$.post('https://golf-courses-api.herokuapp.com/courses',
 		local_obj,
 		function(data){
-			closeCourses = JSON.parse(data);
+			courses = JSON.parse(data);
 
-			for (var p in closeCourses.courses){
-				console.log(closeCourses.courses[p].name);
+			for (var p in courses.courses){
+				console.log(courses.courses[p].name);
 			}
 
-			console.log(closeCourses);
+			loadCourseNames();
+			loadCourseChoiceOptions();
+			loadTeeChoiceOptions();
+
 		}
 	);
 }
 
-loadMe();
+
+
+function loadCourseNames(){
+	for (var p in courses.courses){
+		courseNames.push(courses.courses[p].name);
+	}
+}
+
+
+
+function loadCourseChoiceOptions(){
+	for (var name in courseNames){
+		courseOptions += '<option value="' +
+			courseNames[name]  +  '" >' +
+			courseNames[name] + '</option>';
+	}
+	$('#course-name-selection').html(courseOptions);
+}
+
+
+
+function loadTeeChoiceOptions(){
+
+}
