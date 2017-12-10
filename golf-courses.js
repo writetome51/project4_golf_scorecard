ad93625b36bbe3b597d21ec7990c8907e735f02b
Steve Thompson <writetome51@gmail.com>
2017-12-09 23:27:52 -0700

    Tee, Par, and Handicap rows all fill in automatically

--- golf-courses.js
+++ golf-courses.js
@@ -17,11 +17,8 @@ var hcpOfCurrentTeeForEachHole=[];
 
 
 $(document).ready(function(){
-
 	loadEverything();
-
 });
-
 
 
 
@@ -31,7 +28,7 @@ function loadEverything(){
 		function(data){
 			loadAllCourseData(data);
 
-			loadEvents();
+		//	loadEvents();
 		}
 	);
 }
@@ -149,11 +146,13 @@ function fillTeeRow(){
 
 function fillParRow(){
 	fillHoleCells('par-row', parOfCurrentTeeForEachHole);
+	fillAllParTotals();
 }
 
 
 function fillHandicapRow(){
 	fillHoleCells('handicap-row', hcpOfCurrentTeeForEachHole);
+	fillAllHandicapTotals();
 }
 
 
@@ -177,11 +176,28 @@ function fillHoleCells(rowClass, dataForCells){
 }
 
 
+
+function fillAllParTotals(){
+	fillTotalCell('par-cell-out', parOfCurrentTeeForEachHole, [0,9]);
+	fillTotalCell('par-cell-in', parOfCurrentTeeForEachHole, [0,9]);
+	var arr = [ Number($('#par-cell-out').text()) , Number($('#par-cell-in').text()) ];
+	fillTotalCell('par-cell-total', arr, [0, arr.length]);
+}
+
+
 function fillAllTeeTotals(){
 	fillTotalCell('tee-cell-out', yardagesOfCurrentTeeForEachHole, [0,9]);
 	fillTotalCell('tee-cell-in', yardagesOfCurrentTeeForEachHole, [0,9]);
 	var arr = [ Number($('#tee-cell-out').text()) , Number($('#tee-cell-in').text()) ];
 	fillTotalCell('tee-cell-total', arr, [0, arr.length]);
+}
+
+
+function fillAllHandicapTotals(){
+	fillTotalCell('handicap-cell-out', hcpOfCurrentTeeForEachHole, [0,9]);
+	fillTotalCell('handicap-cell-in', hcpOfCurrentTeeForEachHole, [0,9]);
+	var arr = [ Number($('#handicap-cell-out').text()) , Number($('#handicap-cell-in').text()) ];
+	fillTotalCell('handicap-cell-total', arr, [0, arr.length]);
 }
 
 
