6dea9a4942ac5ce4068ee3c78e5673cf37df9ca8
Steve Thompson <writetome51@gmail.com>
2017-12-14 15:43:34 -0700

    new commit

--- golf-courses.js
+++ golf-courses.js
@@ -10,10 +10,13 @@ var currentTeeName;
 var currentTeeIndex;
 var currentCourseHref;
 var teeTypes=[];
-var teeYardages;
-var yardagesOfCurrentTeeForEachHole = [];
-var parOfCurrentTeeForEachHole=[];
-var hcpOfCurrentTeeForEachHole=[];
+
+var fetchedDataForEachHole = {
+	yards:[], // actually holds yardage count from tee for each hole.
+	par:[],
+	hcp:[]
+};
+
 var playerStrokes = {
 	player1:[],
 	player2:[],
@@ -77,8 +80,6 @@ function updateTeesAndCells(){
 	loadCourse(currentCourseHref);
 }
 
-// $("div:not(:has(h1))")
-
 
 function loadCurrentCourseIndex(){
 	currentCourseIndex =  $('#course-name-options').val();
@@ -95,7 +96,6 @@ function loadCurrentCourseHref(){
 function loadCourse(href){
 	$.get(href, function(data){
 		course = data.course;
-	//	console.log(course);
 
 		loadTeeTypes();
 		loadTeeNames();
@@ -133,6 +133,11 @@ function loadTeeNameOptions(){
 function updateCells(){
 	loadCurrentTee();
 	loadDataOfCurrentTeeForEachHole();
+	fillRowsRepresentingFetchedData();
+}
+
+
+function fillRowsRepresentingFetchedData(){
 	fillTeeRow();
 	fillParRow();
 	fillHandicapRow();
@@ -146,20 +151,20 @@ function loadCurrentTee(){
 
 
 function fillTeeRow(){
-	fillHoleCells('tee-row', yardagesOfCurrentTeeForEachHole);
-	fillAllTeeTotals();
+	fillHoleCells('yards-row', fetchedDataForEachHole.yards);
+	fillTotalCells('yards');
 }
 
 
 function fillParRow(){
-	fillHoleCells('par-row', parOfCurrentTeeForEachHole);
-	fillAllParTotals();
+	fillHoleCells('par-row', fetchedDataForEachHole.par);
+	fillTotalCells('par');
 }
 
 
 function fillHandicapRow(){
-	fillHoleCells('handicap-row', hcpOfCurrentTeeForEachHole);
-	fillAllHandicapTotals();
+	fillHoleCells('hcp-row', fetchedDataForEachHole.hcp);
+	fillTotalCells('hcp');
 }
 
 
@@ -183,31 +188,15 @@ function fillHoleCells(rowClass, dataForCells){
 }
 
 
-
-function fillAllParTotals(){
-	fillTotalCell('par-cell-out', parOfCurrentTeeForEachHole, [0,9]);
-	fillTotalCell('par-cell-in', parOfCurrentTeeForEachHole, [0,9]);
-	var arr = [ Number($('#par-cell-out').text()) , Number($('#par-cell-in').text()) ];
-	fillTotalCell('par-cell-total', arr, [0, arr.length]);
+function fillTotalCells(rowName){
+	fillTotalCell(rowName + '-cell-out',  fetchedDataForEachHole[rowName], [0,9]);
+	fillTotalCell(rowName + '-cell-in',  fetchedDataForEachHole[rowName], [0,9]);
+	var arr = [
+		Number($('#' + rowName + '-cell-out').text()),
+		Number($('#' + rowName + '-cell-in').text())
+	];
+	fillTotalCell(rowName + '-cell-total', arr, [0, arr.length]);
 }
-
-
-function fillAllTeeTotals(){
-	fillTotalCell('tee-cell-out', yardagesOfCurrentTeeForEachHole, [0,9]);
-	fillTotalCell('tee-cell-in', yardagesOfCurrentTeeForEachHole, [0,9]);
-	var arr = [ Number($('#tee-cell-out').text()) , Number($('#tee-cell-in').text()) ];
-	fillTotalCell('tee-cell-total', arr, [0, arr.length]);
-}
-
-
-function fillAllHandicapTotals(){
-	fillTotalCell('handicap-cell-out', hcpOfCurrentTeeForEachHole, [0,9]);
-	fillTotalCell('handicap-cell-in', hcpOfCurrentTeeForEachHole, [0,9]);
-	var arr = [ Number($('#handicap-cell-out').text()) , Number($('#handicap-cell-in').text()) ];
-	fillTotalCell('handicap-cell-total', arr, [0, arr.length]);
-}
-
-
 
 
 function fillTotalCell(cellID, arrayToTally, range){
@@ -232,9 +221,9 @@ function getTally(arrayToTally){
 
 function loadDataOfCurrentTeeForEachHole(){
 
-	yardagesOfCurrentTeeForEachHole = [];
-	parOfCurrentTeeForEachHole=[];
-	hcpOfCurrentTeeForEachHole=[];
+	for (var p in fetchedDataForEachHole){
+		fetchedDataForEachHole[p] = [];
+	}
 
 	for (var hole=0, thisHole;  hole < course.holes.length;  ++hole){
 		thisHole = course.holes[hole];
@@ -242,32 +231,25 @@ function loadDataOfCurrentTeeForEachHole(){
 		for (var tee_box=0, currentTee; tee_box < thisHole.tee_boxes.length; ++tee_box){
 			currentTee = thisHole.tee_boxes[tee_box];
 			if (currentTee.tee_type === currentTeeName){
-				yardagesOfCurrentTeeForEachHole.push(currentTee.yards);
-				parOfCurrentTeeForEachHole.push(currentTee.par);
-				hcpOfCurrentTeeForEachHole.push(currentTee.hcp);
+				fetchedDataForEachHole.yards.push(currentTee.yards);
+				fetchedDataForEachHole.par.push(currentTee.par);
+				fetchedDataForEachHole.hcp.push(currentTee.hcp);
 				break;
 			}
 		}
-		if ( ! yardagesOfCurrentTeeForEachHole[hole]){
-			yardagesOfCurrentTeeForEachHole[hole] = ' - ';
-		}
-		if ( ! parOfCurrentTeeForEachHole[hole]){
-			parOfCurrentTeeForEachHole[hole] = ' - ';
-		}
-		if ( ! hcpOfCurrentTeeForEachHole[hole]){
-			hcpOfCurrentTeeForEachHole[hole] = ' - ';
+		for (var p in fetchedDataForEachHole){
+			if ( ! fetchedDataForEachHole[p][hole]){
+				fetchedDataForEachHole[p][hole] = ' - ';
+			}
 		}
 	}
 
-	while (yardagesOfCurrentTeeForEachHole.length < 18){
-		yardagesOfCurrentTeeForEachHole.push(' - ');
+	for (var p in fetchedDataForEachHole){
+		while (fetchedDataForEachHole[p].length < 18){
+			fetchedDataForEachHole[p].push(' - ');
+		}
 	}
-	while (parOfCurrentTeeForEachHole.length < 18){
-		parOfCurrentTeeForEachHole.push(' - ');
-	}
-	while (hcpOfCurrentTeeForEachHole.length < 18){
-		hcpOfCurrentTeeForEachHole.push(' - ');
-	}
+
 }
 
 
@@ -304,11 +286,26 @@ function loadEvents(){
 			$(this).val(0);
 		}
 
-		var parent = $(this).closest('.strokes-input-cell');
-		var id = parent.attr('id');
-		var parts = id.split('-');
-		var player = parts[0];
+		var player = getPlayer($(this));
 		updateStrokeTotals(player);
 	});
+}
 
+
+function getPlayer(obj){
+	var id = getCellID(obj);
+	return playerNameInID(id);
+}
+
+
+function getCellID(obj){
+	var parent = obj.closest('.strokes-input-cell');
+	return parent.attr('id');
+}
+
+
+function playerNameInID(id){
+	var parts = id.split('-');
+	var player = parts[0];
+	return player;
 }
